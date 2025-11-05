9eedebcddaf6b2bdeccecdf7233783d0d5863d46
S23B0121-AqifAddin <s23b0121@siswa.umk.edu.my>
2025-11-05 11:47:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.set_page_config(page_title="Bangladesh Motorbike Accident")
+
+# Define each page
+page_1 = st.Page("page1.py", title="Page 1")
+page_2 = st.Page("page2.py", title="Page 2")
+page_3 = st.Page("page3.py", title="Page 3")
+
+# Create navigation menu
+pg = st.navigation(
+    {
+        "Menu": [page_1, page_2, page_3]
+    }
+)
+
+pg.run()
